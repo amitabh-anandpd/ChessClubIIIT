55f77c276a9b71c6715644e3fda6e9d318e55f77
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-09-28 14:38:35 +0530

    Renamed

--- IIITChessClub/asgi.py
+++ IIITChessClub/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for IIITLeaderboard project.
+ASGI config for IIITChessClub project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IIITLeaderboard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IIITChessClub.settings')
 
 application = get_asgi_application()
