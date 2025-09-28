b8b85b4673b658946f3b58d42dc62b0873e7292f
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-09-29 01:00:26 +0530

    Added admin for accounts

---
+++ accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import User, UserProfile
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email',)
+    search_fields = ('username', 'email',)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating', 'rank')
+    search_fields = ('user', 'rank',)
