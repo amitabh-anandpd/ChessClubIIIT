1cc11cc1f9b225b3377c247a3cc901c2346e3ace
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-09-29 11:00:55 +0530

    Added last rank and rating details

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import transaction
 
 class User(AbstractUser):
     
@@ -20,7 +21,9 @@ class User(AbstractUser):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     rating = models.IntegerField(default=600)
+    last_rating = models.IntegerField(default=600)
     rank = models.IntegerField(default=0)
+    last_rank = models.IntegerField(default=0)
     chessdotcom = models.CharField(max_length=64, blank=True, null=True)
     lichess = models.CharField(max_length=64, blank=True, null=True)
     
@@ -36,3 +39,33 @@ class UserProfile(models.Model):
             "chessdotcom": self.chessdotcom,
             "lichess": self.lichess,
         }
+    
+    def save(self, *args, **kwargs):
+        with transaction.atomic():
+            if self.pk:
+                old = UserProfile.objects.get(pk=self.pk)
+                if old.rating != self.rating:
+                    self.last_rating = old.rating
+                if old.rank != self.rank:
+                    self.last_rank = old.rank
+            else:
+                self.last_rating = self.rating
+                self.last_rank = self.rank
+
+            super().save(*args, **kwargs)
+    
+    @property
+    def rating_change(self):
+        return self.rating - self.last_rating
+
+    @property
+    def rank_change(self):
+        return self.last_rank - self.rank
+
+    @property
+    def moved_up_in_rank(self):
+        return self.rank < self.last_rank
+
+    @property
+    def moved_down_in_rank(self):
+        return self.rank > self.last_rank
