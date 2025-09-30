1e94afa2ad5ed0423127156c27ffd4c0ea285b01
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-09-30 19:35:35 +0530

    Debug=False

--- IIITChessClub/settings.py
+++ IIITChessClub/settings.py
@@ -27,7 +27,7 @@ if env_path.exists():
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1', 'localhost']
 
--- IIITChessClub/urls.py
+++ IIITChessClub/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .views import home, tournaments, newsletters
@@ -25,4 +27,5 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('tournaments/', tournaments, name='tournaments'),
     path('newsletters/', newsletters, name='newsletters'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#handler404 = 'IIITChessClub.views.custom_404_view'
