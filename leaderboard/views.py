54cfbc95e3b86ca2606853068d0726cb985c4dbd
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-10-01 15:06:57 +0530

    Added rank update logic

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -11,20 +11,21 @@ def leaderboard(request):
         for index, user in enumerate(users, start=1):
             profile = user.profile
             current_rank = index
-            last_rank = profile.last_rank or current_rank
-            rank_change = last_rank - current_rank
+            
+            if profile.rank != current_rank:
+                profile.last_rank = profile.rank or current_rank
+                profile.rank = current_rank
+                profile.save(update_fields=['rank', 'last_rank'])
+                
+            rank_change = (profile.last_rank or current_rank) - current_rank
 
             leaderboard_data.append({
                 "user": user,
                 "current_rank": current_rank,
                 "rank_change": rank_change,
-                "moved_up": current_rank < last_rank,
-                "moved_down": current_rank > last_rank,
+                "moved_up": current_rank < profile.last_rank,
+                "moved_down": current_rank > profile.last_rank,
                 "rating_change": profile.rating - profile.last_rating,
             })
-
-            if profile.last_rank != current_rank:
-                profile.last_rank = current_rank
-                profile.save(update_fields=['last_rank'])
     return render(request, 'leaderboard.html', {"leaderboard": leaderboard_data})
     
